ff06bed93c9b6bb8bc04570235a136c8075bf9b6
Joffrey Lemery <joffrey.lemery.jl@gmail.com>
2023-02-13 19:00:21 +0100

    Add the empty script for dld image and changes in dockerfiles 	modified:   backend_dataprocessing/Dockerfile 	modified:   backend_dataprocessing/main.py 	modified:   backend_dataprocessing/requierments.txt 	modified:   visualization/dld_images_GBIF.ipynb

--- backend_dataprocessing/main.py
+++ backend_dataprocessing/main.py
@@ -9,12 +9,11 @@ from fastapi import File
 from fastapi import FastAPI
 from fastapi import UploadFile
 import numpy as np
-from PIL import Image
 import datetime as dt
 
 
 import update_data as upd
-import dataprocessing as dpg
+import dataprocessing_from_csv_MO as dpg
 
 
 app = FastAPI()
@@ -22,9 +21,16 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome from the API of data management"}
+    return {"message": "Welcome from the API of dataprocessing"}
 
 
+#activate the update calls to MO and the dataprocessing to append the csv-files
+@app.post("/{date}")
+def get_image(date: str, file: UploadFile = File(...)):
+
+    return 
+
+#activate the update of models training
 @app.post("/{date}")
 def get_image(date: str, file: UploadFile = File(...)):
 
