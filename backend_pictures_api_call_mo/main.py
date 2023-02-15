735090289552b1e785a7cbef3b3238bf4ee3e5ef
JoffreyLemery <joffrey.lemery.jl@gmail.com>
2023-02-15 22:18:30 +0000

    Ajout de la fonctionnalité de déplacement du fichier CSV
    	modified:   backend_pictures_api_call_mo/Dockerfile
    	new file:   backend_pictures_api_call_mo/config_user.py
    	modified:   backend_pictures_api_call_mo/main.py

--- backend_pictures_api_call_mo/main.py
+++ backend_pictures_api_call_mo/main.py
@@ -3,11 +3,49 @@ import requests
 import os
 import zipfile
 import time
+from pathlib import Path
+import shutil
 
 print('hello')
 # df = pd.read_csv('/var/lib/docker/volumes/Image_MO/_data/images_names.csv')
 
-df = pd.read_csv('image_mo_container/images_names.csv')
+# create a Path object with the path to the file
+path_file = Path('images_names.csv')
+path_volume = 'volume_backend'
+
+
+def move_file(path):
+    """path could either be relative or absolute. """
+    # check if file or directory exists
+    if os.path.isfile(path) or os.path.islink(path) or os.path.isdir(path):
+        # move file
+        shutil.move(path, path_volume)
+    else:
+        raise ValueError("Path {} is not a file or dir.".format(path))
+
+
+def delete_file(path):
+    """path could either be relative or absolute. """
+    # check if file or directory exists
+    if os.path.isfile(path) or os.path.islink(path):
+        # remove file
+        os.remove(path)
+    elif os.path.isdir(path):
+        # remove directory and all its content
+        shutil.rmtree(path)
+    else:
+        raise ValueError("Path {} is not a file or dir.".format(path))
+
+
+if os.path.isfile(path_file) == True:
+    move_file(path_file)
+    delete_file(path_file)
+elif os.path.isfile(path_volume/path_file) == True:
+    print('CSV present in the volume')
+else : raise ValueError('CSV file is absent from build repertory (host machine) or volume : images_names.csv')
+
+
+df = pd.read_csv('volume_backend/images_names.csv')
 
 df = df.sample(200)
 
