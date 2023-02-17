8cdabace6888b5a2dfe0f5991e93fc5cf36d86e7
JoffreyLemery <joffrey.lemery.jl@gmail.com>
2023-02-17 09:17:30 +0000

    Fixd_size_issue_test_1000 	modified:   backend_pictures_api_call_mo/main.py

--- backend_pictures_api_call_mo/main.py
+++ backend_pictures_api_call_mo/main.py
@@ -56,7 +56,7 @@ PATHs = 'https://images.mushroomobserver.org/320/'
 
 # print('hello4')
 
-for k in range(10):
+for k in range(1000):
     try:
         uplo_from_url(PATHs+'{0}.jpg'.format(df.index.values[k]))
     except:
