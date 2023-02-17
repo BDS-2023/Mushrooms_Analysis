d92bdfc59c0cb06d847a37827f4b11eaa7d6c5cb
JoffreyLemery <joffrey.lemery.jl@gmail.com>
2023-02-17 08:43:58 +0000

    lighter df in docker 	modified:   backend_pictures_api_call_mo/main.py

--- backend_pictures_api_call_mo/main.py
+++ backend_pictures_api_call_mo/main.py
@@ -11,62 +11,58 @@ print('hello1')
 
 df = pd.read_csv('images_names.csv',usecols=['image_id'],index_col=0)
 
-# session = boto3.Session()
-# s3 = session.resource('s3')
+session = boto3.Session()
+s3 = session.resource('s3')
 
 
-# my_bucket = s3.Bucket('imagemobucket')
+my_bucket = s3.Bucket('imagemobucket')
 
-# alr_in = []
-# for objects in my_bucket.objects.filter(Prefix="image_mo/"):
-#     try:
-#         alr_in.append(int(str(objects.key).split('/')[-1][:-4]))
-#     except:
-#         pass
+alr_in = []
+for objects in my_bucket.objects.filter(Prefix="image_mo/"):
+    try:
+        alr_in.append(int(str(objects.key).split('/')[-1][:-4]))
+    except:
+        pass
 
-# alr_in = np.array(alr_in)
+alr_in = np.array(alr_in)
 
-# print(alr_in)
+print(alr_in)
 
-# print('hello2')
+print('hello2')
 
-# def uplo_from_url(url):
-#     bucket_name = 'imagemobucket'
-#     key_doss = 'image_mo/'
-#     key = key_doss + url.split('/')[-1]
+def uplo_from_url(url):
+    bucket_name = 'imagemobucket'
+    key_doss = 'image_mo/'
+    key = key_doss + url.split('/')[-1]
     
 
-#     req_for_image = requests.get(url, stream=True)
-#     file_object_from_req = req_for_image.raw
-#     req_data = file_object_from_req.read()
+    req_for_image = requests.get(url, stream=True)
+    file_object_from_req = req_for_image.raw
+    req_data = file_object_from_req.read()
 
-#     # Do the actual upload to s3
-#     s3.Bucket(bucket_name).put_object(Key=key, Body=req_data)
+    # Do the actual upload to s3
+    s3.Bucket(bucket_name).put_object(Key=key, Body=req_data)
     
-    
 
 
-
-
-# df = df.drop_duplicates(subset='image_id')
-# df = df[~df.image_id.isin(alr_in)]
+df = df[~df.index.isin(alr_in)]
 
 print('hello3')
 
-# PATHs = 'https://images.mushroomobserver.org/320/'
-# path_temp = df.image_id.values
-# urls_all = [PATHs+'{0}.jpg'.format(path) for path in path_temp]
+PATHs = 'https://images.mushroomobserver.org/320/'
+path_temp = df.image_id.values
+urls_all = [PATHs+'{0}.jpg'.format(path) for path in path_temp]
 
 # print('hello4')
 
-# for k in range(10):
-#     try:
-#         uplo_from_url(urls_all[k])
-#     except:
-#         print('oups')
+for k in range(10):
+    try:
+        uplo_from_url(urls_all[k])
+    except:
+        print('oups')
 
 
 
 
-# # docker image build . -t my_image:latest
-# # docker container run -it --name plop --mount type=volume,src=Image_MO,dst=/app/image_mo_container my_image:latest
+# # # docker image build . -t my_image:latest
+# # # docker container run -it --name plop --mount type=volume,src=Image_MO,dst=/app/image_mo_container my_image:latest
